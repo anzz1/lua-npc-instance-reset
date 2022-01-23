d95b4315dbd805760d696317b28d0f2509977287
anzz1 <anzz1@live.com>
2022-01-23 20:41:49 +0200

    Update npc_instance_reset.lua

--- npc_instance_reset.lua
+++ npc_instance_reset.lua
@@ -16,6 +16,7 @@ local FILE_NAME = string.match(debug.getinfo(1,'S').source, "[^/\\]*.lua$")
 local function OnGossipHello(event, player, unit) 
 	player:GossipMenuAddItem(0, "I would like to remove my instance saves.", 1, 1, false, "Are you sure you want to reset all instances?")
     player:GossipSendMenu(1, unit)
+    unit:PerformEmote(1)
     return true
 end    
 
@@ -24,6 +25,7 @@ local function OnGossipSelect(event, player, unit, sender, intid, code)
     	player:UnbindAllInstances()
     	unit:SendUnitWhisper("Your instances have been reset.", 0, player)
         player:GossipComplete()
+        unit:PerformEmote(23)
     end
     return true
 end
